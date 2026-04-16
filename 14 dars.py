2c92f6e8e184559f236fd20063e2db48e136b5f9
amirov922020-crypto <amirov922020@gmail.com>
2026-04-16 07:34:23 +0300

    yangi fayl
    14 darsdan (mohirdev)

---
+++ 14 dars.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#"""
+#Spyder Editor
+
+#This is a temporary script file.
+#"""
+
+talaba_1 = {}
+
+talaba_1['ism'] = 'qobil rasulov'
+talaba_1['kurs'] = 3
+talaba_1['yosh'] = 20
+print(talaba_1)
+print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
