205f92f3b76f8f19f40df7ee19c85fc3bb9c6eb5
amirov922020-crypto <amirov922020@gmail.com>
2026-04-16 07:38:21 +0300

    14 dars yangilandi

--- 14 dars.py
+++ 14 dars.py
@@ -12,3 +12,15 @@ talaba_1['kurs'] = 3
 talaba_1['yosh'] = 20
 print(talaba_1)
 print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+
+
+
+telefonlar = {
+    'ali':'iphone x',
+    'vali':'galaxy s9',
+    'olim':'mi 10 pro',
+    'orif':'nokia 3310'
+    }
+
+phone = telefonlar['ali']
+print(f"Alining telefoni {phone}")
